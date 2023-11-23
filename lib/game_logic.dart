bf1d55c718a0f9c11fc319b601e7404f2df6aaae
Hassan <syedhassanraza0311@gmail.com>
2023-11-23 14:10:32 +0500

    Tic Tac Toe App

---
+++ lib/game_logic.dart
@@ -0,0 +1,31 @@
+class Player {
+  static const x = "X";
+  static const o = "O";
+  static const emty = "";
+}
+
+class Game {
+  static final boardLenght = 9;
+  static final boardSize = 100.0;
+
+  List<String>? board;
+  static List<String>? initGameBoard() =>
+      List.generate(boardLenght, (index) => Player.emty);
+
+  bool winnerCheck(
+      String player, int index, List<int> scoreBoard, int gridsize) {
+    int row = index ~/ 3;
+    int col = index % 3;
+    int score = player == "X" ? 1 : -1;
+
+    scoreBoard[row] += score;
+    scoreBoard[gridsize + col] += score;
+    if (row == col) scoreBoard[2 * gridsize] += score;
+    if (gridsize - 1 - col == row) scoreBoard[2 * gridsize + 1] += score;
+
+    if (scoreBoard.contains(3) || scoreBoard.contains(-3)) {
+      return true;
+    }
+    return false;
+  }
+}
