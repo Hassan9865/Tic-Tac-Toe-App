9837e6466207287f82db5cdd7a08cf2e5ade280b
Hassan <syedhassanraza0311@gmail.com>
2024-11-24 09:15:48 +0500

    tic tac toe

--- lib/game_logic.dart
+++ lib/game_logic.dart
@@ -1,31 +1,142 @@
-class Player {
-  static const x = "X";
-  static const o = "O";
-  static const emty = "";
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe_app/components/color.dart';
+
+void main() => runApp(TicTacToeApp());
+
+class TicTacToeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TicTacToeGame(),
+    );
+  }
 }
 
-class Game {
-  static final boardLenght = 9;
-  static final boardSize = 100.0;
+class TicTacToeGame extends StatefulWidget {
+  @override
+  _TicTacToeGameState createState() => _TicTacToeGameState();
+}
 
-  List<String>? board;
-  static List<String>? initGameBoard() =>
-      List.generate(boardLenght, (index) => Player.emty);
+class _TicTacToeGameState extends State<TicTacToeGame> {
+  List<String> board = List.filled(9, '');
+  String currentPlayer = 'X';
+  String winner = '';
 
-  bool winnerCheck(
-      String player, int index, List<int> scoreBoard, int gridsize) {
-    int row = index ~/ 3;
-    int col = index % 3;
-    int score = player == "X" ? 1 : -1;
+  void resetGame() {
+    setState(() {
+      board = List.filled(9, '');
+      currentPlayer = 'X';
+      winner = '';
+    });
+  }
 
-    scoreBoard[row] += score;
-    scoreBoard[gridsize + col] += score;
-    if (row == col) scoreBoard[2 * gridsize] += score;
-    if (gridsize - 1 - col == row) scoreBoard[2 * gridsize + 1] += score;
+  void handleTap(int index) {
+    if (board[index].isNotEmpty || winner.isNotEmpty) return;
 
-    if (scoreBoard.contains(3) || scoreBoard.contains(-3)) {
-      return true;
+    setState(() {
+      board[index] = currentPlayer;
+      if (checkWinner()) {
+        winner = '$currentPlayer wins!';
+      } else if (!board.contains('')) {
+        winner = 'It\'s a draw!';
+      } else {
+        currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+      }
+    });
+  }
+
+  bool checkWinner() {
+    const winPatterns = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+      [0, 4, 8], [2, 4, 6], // Diagonals
+    ];
+    for (var pattern in winPatterns) {
+      if (board[pattern[0]] == currentPlayer &&
+          board[pattern[1]] == currentPlayer &&
+          board[pattern[2]] == currentPlayer) {
+        return true;
+      }
     }
     return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MyColor.color1,
+      appBar: AppBar(
+        backgroundColor: MyColor.color2,
+        centerTitle: true,
+        title: Text(
+          "Tic Tac Toe Game",
+          style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: MediaQuery.of(context).size.width / 15),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "It's $currentPlayer turn".toUpperCase(),
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: MediaQuery.of(context).size.width / 9),
+              ),
+              GridView.builder(
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  mainAxisSpacing: 8.0,
+                  crossAxisSpacing: 8.0,
+                  crossAxisCount: 3,
+                ),
+                itemCount: 9,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () => handleTap(index),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: MyColor.color2,
+                      ),
+                      child: Center(
+                        child: Text(
+                          board[index],
+                          style: TextStyle(
+                              fontSize: MediaQuery.of(context).size.width / 13,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.pink),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+              SizedBox(height: 20),
+              if (winner.isNotEmpty)
+                Text(
+                  winner,
+                  style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width / 17,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: resetGame,
+                child: Text('Reset Game'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,126 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:tic_tac_toe_app/components/color.dart';
-import 'package:tic_tac_toe_app/game_logic.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class ApnaGame extends StatefulWidget {
+  const ApnaGame({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<ApnaGame> createState() => _ApnaGameState();
 }
 
-class _HomePageState extends State<HomePage> {
-  String value = "X";
-  bool gameOver = false;
-  int turn = 0;
-  String result = "";
-
-  List<int> scoreBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-  Game game = Game();
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    game.board = Game.initGameBoard();
-    print(game.board);
-  }
-
+class _ApnaGameState extends State<ApnaGame> {
   @override
   Widget build(BuildContext context) {
-    double boardWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: MyColor.color1,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "It's $value turn".toUpperCase(),
-              style: TextStyle(color: Colors.white, fontSize: 50),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: boardWidth,
-              height: boardWidth,
-              child: GridView.count(
-                  crossAxisCount: Game.boardLenght ~/ 3,
-                  padding: EdgeInsets.all(16.0),
-                  mainAxisSpacing: 8.0,
-                  crossAxisSpacing: 8.0,
-                  children: List.generate(Game.boardLenght, (index) {
-                    return InkWell(
-                      onTap: gameOver
-                          ? null
-                          : () {
-                              if (game.board![index] == "") {
-                                setState(() {
-                                  game.board![index] = value;
-                                  turn++;
-                                  gameOver = game.winnerCheck(
-                                      value, index, scoreBoard, 3);
-                                  if (gameOver) {
-                                    result = "$value is the Winner";
-                                  } else if (!gameOver && turn == 9) {
-                                    result = "It's a Draw";
-                                    gameOver = true;
-                                  }
-                                  if (value == "X")
-                                    value = "0";
-                                  else
-                                    value = "X";
-                                });
-                              }
-                            },
-                      child: Container(
-                        width: Game.boardSize,
-                        height: Game.boardSize,
-                        decoration: BoxDecoration(
-                          color: MyColor.color2,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Center(
-                          child: Text(
-                            game.board![index],
-                            style: TextStyle(
-                              color: game.board![index] == "x"
-                                  ? Colors.blue
-                                  : Colors.pink,
-                              fontSize: 64.0,
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  })),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              result,
-              style: TextStyle(color: Colors.white, fontSize: 50),
-            ),
-            ElevatedButton.icon(
-                onPressed: () {
-                  setState(() {
-                    game.board = Game.initGameBoard();
-                    value = "X";
-                    gameOver = false;
-                    turn = 0;
-                    result = "";
-                    scoreBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-                  });
-                },
-                icon: Icon(Icons.replay),
-                label: Text("repeat the game"))
-          ],
-        ),
+      appBar: AppBar(
+        title: Text("Tic Tac Toe Game"),
+        centerTitle: true,
       ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          GridView.builder(
+              shrinkWrap: true,
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 9),
+              itemBuilder: (context, index) {
+                return Container();
+              })
+        ],
+      )),
     );
   }
 }
